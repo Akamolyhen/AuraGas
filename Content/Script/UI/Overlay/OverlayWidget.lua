bbeb10c534c5d0d683ed3f2df2b0429fb0a8db60
HenryMorgan <aka_wannabe@163.com>
2024-09-03 23:04:36 +0800

    [feat] EffectMessageOverlay UI && Animation

--- Content/Script/UI/Overlay/OverlayWidget.lua
+++ Content/Script/UI/Overlay/OverlayWidget.lua
@@ -27,12 +27,17 @@ function M:WidgetControllerSet()
     WidgetController.MessageWidgetRowDelegate:Add(self,self.OnGetMessageWidgetRow)
     self.WBP_HealthGlobe:SetWidgetController(self.WidgetController)
     self.WBP_ManaGlobe:SetWidgetController(self.WidgetController)
-    
 end
 
 ---@param Row FUIWidgetRow
 function M:OnGetMessageWidgetRow(Row)
-    UE.UKismetSystemLibrary.PrintString(self,Row.Message)
+    ---@type WBP_EffectMessage_C
+    local EffectMessageWidget = UE.UWidgetBlueprintLibrary.Create(self,Row.MessageWidget)
+    EffectMessageWidget:SetImgAndText(Row.Image,Row.Message)
+    local ViewportSize = UE.UWidgetLayoutLibrary.GetViewportSize(self)
+    local Position = UE.FVector2D(ViewportSize.X*0.1,ViewportSize.Y*0.6)
+    EffectMessageWidget:SetPositionInViewport(Position,true)
+    EffectMessageWidget:AddToViewport()
 end
 
 return M
